f0d9e31b36c18e50d2f3b603738dcd23d6fdbd1a
rhoggSugarcrm <rhogg@sugarcrm.com>
2015-09-01 13:39:23 -0700

    Move all the aliases to one place

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -91,3 +91,17 @@ alias "validateApache"="httpd -t";
 
 # gjslint
 alias gjslint='/usr/local/bin/gjslint --flagfile=~/.gjslint-config';
+# for colors
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+
+# grep colors
+export GREP_OPTIONS='--color=auto'
+
+# alias for listing
+alias ll='ls -lah'
+alias la='ls -a'
+alias l='ls -lh'
+
+# alias for disk tools
+alias du='du -h'

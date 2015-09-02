1b0e0ddd7bed97825f854a5dacc3fcdad1f5f182
rhoggSugarcrm <rhogg@sugarcrm.com>
2015-09-02 15:52:29 -0700

    Add FIREFOX_BIN environment variable so Karma knows where to look for Firefox

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -4,6 +4,9 @@ export EDITOR="emacs";
 export VISUAL="emacs";
 export GIT_EDITOR="/usr/local/bin/emacs"; # has to open in the foreground
 
+# Set up Firefox for Karma
+export FIREFOX_BIN="$HOME/Applications/Firefox.app/Contents/MacOS/firefox-bin";
+
 # your projects folder
 export PROJECTS=~/src
 

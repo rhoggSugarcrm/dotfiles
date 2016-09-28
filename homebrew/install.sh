d7f088077d69123bee7c7bf2b5175ccc6f0ae040
rhoggSugarcrm <rhogg@sugarcrm.com>
2016-09-28 13:41:32 -0400

    Use brew bundle -v

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,15 +4,10 @@
 #
 # If we're on a Mac, let's install http://brew.sh and setup some of the common
 # dependencies needed (or at least desired) for us.
-#
 
-if [ "$(uname -s)" != "Darwin" ]
-then
-  return
-fi
-
+# FIXME: this should support Linuxbrew too
 # Check if we already have Homebrew installed
 type -P brew &>/dev/null ||
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 
-brew bundle
+brew bundle -v;

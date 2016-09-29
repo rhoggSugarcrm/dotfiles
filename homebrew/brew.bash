69950ad4bdee75ef2860e6f691e8b91f91818d1e
rhoggSugarcrm <rhogg@sugarcrm.com>
2016-09-29 17:39:35 -0400

    Prevent homebrew auto-updating. Use textmate as editor

--- homebrew/brew.bash
+++ homebrew/brew.bash
@@ -1,5 +1,8 @@
 type -P brew &>/dev/null || return
 
+export HOMEBREW_NO_AUTO_UPDATE=1;
+export HOMEBREW_DEVELOPER=1;
+
 # Sources a file
 # Accepts 1 param which is the file we want include
 function __open_war_loadifexists() {

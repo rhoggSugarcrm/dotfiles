9707129ff40b3fe0c86770e42a07b630a9da3fe8
Bob Wombat Hogg <rhogg@sugarcrm.com>
2016-10-07 12:16:07 -0400

    Brewing node installs npm by default.

--- node/install.sh
+++ node/install.sh
@@ -4,9 +4,6 @@ if test ! $(which node) -a $(which brew)
 then
   inform 'Installing node for you.'
   brew install node > /tmp/node-install.log
-
-  inform 'Installing npm for you.'
-  curl -L https://npmjs.org/install.sh | sh
 fi
 
 # install some dependencies

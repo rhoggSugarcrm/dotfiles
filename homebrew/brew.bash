05284d1b64e0222d292d9b154d97a7479234250b
Bob Wombat Hogg <rhogg@sugarcrm.com>
2017-02-06 14:39:13 -0800

    Remove grc stuff

--- homebrew/brew.bash
+++ homebrew/brew.bash
@@ -1,8 +1,9 @@
 type -P brew &>/dev/null || return
 
-export HOMEBREW_NO_AUTO_UPDATE=1;
-export HOMEBREW_DEVELOPER=1;
-export HOMEBREW_NO_ANALYTICS=1;
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_DEVELOPER=1
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_VERBOSE=1
 
 # Sources a file
 # Accepts 1 param which is the file we want include
@@ -12,5 +13,3 @@ function __open_war_loadifexists() {
         . $1
     fi
 }
-
-__open_war_loadifexists "`brew --prefix`/etc/grc.bashrc"

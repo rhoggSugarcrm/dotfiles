e114b7290bb528bd94b1f2331b81d66d6b090f9e
Bob Wombat Hogg <rhogg@sugarcrm.com>
2017-05-23 10:43:55 -0700

    Remove unneeded homebrew stuff
    Signed-off-by: Bob Wombat Hogg <rhogg@sugarcrm.com>

--- homebrew/brew.bash
+++ homebrew/brew.bash
@@ -4,12 +4,3 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_DEVELOPER=1
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_VERBOSE=1
-
-# Sources a file
-# Accepts 1 param which is the file we want include
-function __open_war_loadifexists() {
-
-    if [ -f $1 ]; then
-        . $1
-    fi
-}

bb6f47ba9038fb4d82aedf153fc868b067c88ecd
Bob Wombat Hogg <rhogg@sugarcrm.com>
2018-04-25 10:49:06 -0700

    Upgrade PHP version used by MAMP
    Signed-off-by: Bob Wombat Hogg <rhogg@sugarcrm.com>

--- system/path.bash
+++ system/path.bash
@@ -1,3 +1,3 @@
 
 # PATH
-PATH="$HOME/privateBin:/Applications/MAMP/bin/php/php7.1.8/bin:$DOT/bin:/usr/local/bin:/usr/local/sbin:$PATH";
+PATH="$HOME/privateBin:/Applications/MAMP/bin/php/php7.1.12/bin:$DOT/bin:/usr/local/bin:/usr/local/sbin:$PATH";
